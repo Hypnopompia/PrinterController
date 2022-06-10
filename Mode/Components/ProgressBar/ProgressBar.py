9a3c9f2b4aa15c9f6b61f9af64faa7f2b04d280b
TJ Hunter <tj@pronto.io>
2022-06-10 17:00:59 -0600

    add percentage text

--- Mode/Components/ProgressBar/ProgressBar.py
+++ Mode/Components/ProgressBar/ProgressBar.py
@@ -3,14 +3,20 @@ import pygame
 
 class ProgressBar:
     def __init__(self, state, pos, size):
+        self.text = None
         self.state = state
         self.progress = 0
         self.x, self.y = self.pos = pos
         self.width, self.height = self.size = size
 
     def set_progress(self, progress):
+        update = False
         if progress is not None:
+            if progress != self.progress:
+                update = True
             self.progress = progress
+            if update:
+                self.text = self.state.fonts['small'].render(str(int(self.progress)) + "%", True, (255, 255, 255))
         else:
             self.progress = 0
 
@@ -23,6 +29,9 @@ class ProgressBar:
     def render(self, surface):
         width = int(self.width * (self.progress / 100))
         pygame.draw.rect(surface, (57, 136, 207), pygame.Rect(self.x, self.y, self.width, self.height), 1)
-        if (self.progress > 0):
-            pygame.draw.rect(surface, (57, 136, 207), pygame.Rect(self.x, self.y, width, self.height))
+        pygame.draw.rect(surface, (57, 136, 207), pygame.Rect(self.x, self.y, width, self.height))
 
+        if self.text is not None:
+            text_w, text_h = self.text.get_size()
+            x = (self.width // 2) - (text_w // 2)
+            surface.blit(self.text, (x, self.y - 20))
