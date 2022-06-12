b450bd2131df54aafed562cb259e6b58786cf28b
TJ Hunter <tj@pronto.io>
2022-06-11 23:42:51 -0600

    try optimizing camera refresh rate

--- Mode/Components/Camera/Camera.py
+++ Mode/Components/Camera/Camera.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 
 from .ThreadedCamera import ThreadedCamera
@@ -7,18 +9,24 @@ from .. import Component
 class Camera(Component):
     def __init__(self, state, pos, size, source):
         super().__init__(state, pos, size)
-        self.state = None
         self.videoStream = ThreadedCamera(source)
+        self.video_surface = None
+        self.last_grab_time = 0
 
     def process_event(self, event):
         pass
 
     def update(self):
-        self.animate()
+        # self.animate()
+        if time.process_time() > (self.last_grab_time + 0.05):
+            self.last_grab_time = time.process_time()
+            self.video_surface = self.videoStream.grab_surface()
+
+            if self.video_surface is not None:
+                self.video_surface = self.aspect_scale(self.video_surface, self.width, self.height)
+                self.width, self.height = self.video_surface.get_size()
 
     def render(self, surface):
-        video_surface = self.videoStream.grab_surface()
-
-        if video_surface is not None:
-            video_surface = self.aspect_scale(video_surface, self.width, self.height)
-            surface.blit(video_surface, (self.x, self.y))
+        if self.video_surface is not None:
+            surface.blit(self.video_surface, (self.x, self.y))
+            pygame.draw.rect(surface, self.state.colors['border'], pygame.Rect(self.x, self.y, self.width, self.height), 2)  # Border
