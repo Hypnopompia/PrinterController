59690b2cf492003978038d14feeb976fd93de5b5
TJ Hunter <tj@pronto.io>
2022-06-14 14:03:38 -0600

    check fps

--- Mode/Components/TextComponent/TextComponent.py
+++ Mode/Components/TextComponent/TextComponent.py
@@ -33,8 +33,6 @@ class TextComponent(Component):
             self.surface = None
 
     def make_surface(self):
-        self.surface = self.get_new_surface()
-
         offset = 1
         if self.font == "medium":
             offset = 2
@@ -43,12 +41,12 @@ class TextComponent(Component):
 
         text_fg_surface = self.state.fonts[self.font].render(self.text, True, self.state.colors[self.color])
         text_highlight_surface = self.state.fonts[self.font].render(self.text, True, self.state.colors[self.color + "_highlight"])
-        text_surface = self.get_new_surface((text_fg_surface.get_width() + offset, text_fg_surface.get_height() + offset))
-        text_surface.blit(text_highlight_surface, (offset, offset))
-        text_surface.blit(text_fg_surface, (0, 0))
+        self.surface = self.get_new_surface((text_fg_surface.get_width() + offset, text_fg_surface.get_height() + offset))
+        self.surface.blit(text_highlight_surface, (offset, offset))
+        self.surface.blit(text_fg_surface, (0, 0))
 
-        self.text_width = text_surface.get_width()
-        self.text_height = text_surface.get_height()
+        self.text_width = self.surface.get_width()
+        self.text_height = self.surface.get_height()
 
         if self.align == "left":
             self.text_x = 0
@@ -64,10 +62,8 @@ class TextComponent(Component):
         elif self.valign == "bottom":
             self.text_y = self.height - self.text_height
 
-        self.surface.blit(text_surface, (self.text_x, self.text_y))
-
     def render(self, surface):
         if self.surface is None:
             self.make_surface()
 
-        # surface.blit(self.surface, (self.x, self.y))
+        surface.blit(self.surface, (self.x + self.text_x, self.y + self.text_y))
