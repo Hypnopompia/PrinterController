65494189e20328ff66eb9f39484453fc8b356560
TJ Hunter <tj@pronto.io>
2022-06-14 14:34:17 -0600

    testing fps

--- Mode/Components/Component.py
+++ Mode/Components/Component.py
@@ -32,7 +32,7 @@ class Component:
             size = self.size
 
         width, height = size
-        surface = pygame.Surface(size, pygame.SRCALPHA, 32).convert_alpha()
+        surface = pygame.Surface(size, pygame.SRCALPHA, 32)
         # pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(0, 0, width, height), 1)
         return surface
 
