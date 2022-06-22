99d380fba214c9090bf41dd8047d59449bd1287c
TJ Hunter <tj@pronto.io>
2022-06-22 10:28:36 -0600

    add temps to be level screen

--- Mode/LevelMode.py
+++ Mode/LevelMode.py
@@ -1,31 +1,43 @@
 import pygame.draw
 
 from Mode.Mode import Mode
-from .Components import Background, Text, Hud, Button
+from .Components import Background, Text, Hud, Button, Temperature
 
 
 class LevelMode(Mode):
     def __init__(self, state, printer):
         super().__init__(state, printer)
 
-        self.north_west = (220, 80)
-        self.north_east = (480, 80)
-        self.center = (350, 210)
-        self.south_west = (220, 340)
-        self.south_east = (480, 340)
+        self.bed_x, self.bed_y = self.bed_pos = (20, 80)
+        self.bed_size = (380, 380)
+
+        self.north_west = (self.bed_x + 20, self.bed_y + 20)
+        self.north_east = (self.bed_x + 260, self.bed_y + 20)
+        self.center = (self.bed_x + 140, self.bed_y + 140)
+        self.south_west = (self.bed_x + 20, self.bed_y + 260)
+        self.south_east = (self.bed_x + 260, self.bed_y + 260)
 
         self.components = {
             "heading": Text(self.state, "Level Bed", "heading", center=(self.state.window_width // 2, 30)),
             "hud": Hud(self.state),
             "btn_back": Button(self.state, (680, 1), (100, 40), "Back", self.button_back_on_click),
 
-            "btn_north_west": Button(self.state, self.north_west, (100, 100), "", self.button_north_west_on_click, color=self.state.colors['level_btn_border']),
-            "btn_north_east": Button(self.state, self.north_east, (100, 100), "", self.button_north_east_on_click, color=self.state.colors['level_btn_border']),
+            "btn_north_west": Button(self.state, self.north_west, (100, 100), "", self.button_north_west_on_click,
+                                     color=self.state.colors['level_btn_border']),
+            "btn_north_east": Button(self.state, self.north_east, (100, 100), "", self.button_north_east_on_click,
+                                     color=self.state.colors['level_btn_border']),
 
-            "btn_center": Button(self.state, self.center, (100, 100), "", self.button_center_on_click, color=self.state.colors['level_btn_border']),
+            "btn_center": Button(self.state, self.center, (100, 100), "", self.button_center_on_click,
+                                 color=self.state.colors['level_btn_border']),
 
-            "btn_south_west": Button(self.state, self.south_west, (100, 100), "", self.button_south_west_on_click, color=self.state.colors['level_btn_border']),
-            "btn_south_east": Button(self.state, self.south_east, (100, 100), "", self.button_south_east_on_click, color=self.state.colors['level_btn_border']),
+            "btn_south_west": Button(self.state, self.south_west, (100, 100), "", self.button_south_west_on_click,
+                                     color=self.state.colors['level_btn_border']),
+            "btn_south_east": Button(self.state, self.south_east, (100, 100), "", self.button_south_east_on_click,
+                                     color=self.state.colors['level_btn_border']),
+
+            "bed_temp": Temperature(self.state, (420, 180), (360, 40), 'bed', self.toggle_bed_target_temp),
+            "tool_temp": Temperature(self.state, (420, 280), (360, 40), 'tool', self.toggle_tool_target_temp),
+
         }
 
         self.head_pos = None
@@ -64,10 +76,12 @@ class LevelMode(Mode):
             self.components[component].update()
 
     def render(self, surface):
-        pygame.draw.rect(surface, self.state.colors['level_bed_border'], (200, 60, 400, 400), 2);
+        pygame.draw.rect(surface, self.state.colors['level_bed_border'],
+                         (self.bed_x, self.bed_y, self.bed_size[0], self.bed_size[1]), 2);
 
         if self.head_pos is not None:
-            pygame.draw.rect(surface, self.state.colors['level_target'], (self.head_pos[0] + 10, self.head_pos[1] + 10, 80, 80));
+            pygame.draw.rect(surface, self.state.colors['level_target'],
+                             (self.head_pos[0] + 10, self.head_pos[1] + 10, 80, 80))
 
         for component in self.components:
             self.components[component].render(surface)
